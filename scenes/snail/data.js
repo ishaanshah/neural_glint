e3dddf9d1338c7f00f1257312a61d4ea231867e0
Ishaan Shah <ishaan.n.shah@gmail.com>
2024-06-23 19:42:33 +0200

    Add interactive viewer

---
+++ scenes/snail/data.js
@@ -0,0 +1,40 @@
+const data =
+{
+    "imageBoxes": [
+        {
+            "title": "Images",
+            "elements": [
+                {
+                    "title": "Ours",
+                    "version": "-",
+                    "image": "ours.png"
+                },
+                {
+                    "title": "GGN18",
+                    "version": "-",
+                    "image": "gamboa.png"
+                },
+                {
+                    "title": "Reference",
+                    "version": "-",
+                    "image": "gt.png"
+                },
+            ]
+        },
+        {
+            "title": "ꟻLIP",
+            "elements": [
+                {
+                    "title": "Ours",
+                    "version": "-",
+                    "image": "flip_ours.png"
+                },
+                {
+                    "title": "GGN",
+                    "version": "-",
+                    "image": "flip_gamboa.png"
+                },
+            ]
+        }
+    ],
+}
--- static/js/interactive_viewer.js
+++ static/js/interactive_viewer.js
@@ -1,26 +1,11 @@
 function setup() {
-    var local = true;
-
-    content = document.getElementById("content-time");
+    content = document.getElementById("content-ggn18");
 
     scenes = [
-        ["Zero-Day (Glossy)", "scenes/zero-day-(etg)/"],
-        ["Bistro Exterior (Glossy)", "scenes/bistro-exterior-(etg)/"],
-        ["Bistro Interior (Glossy)", "scenes/bistro-interior-(etg)/"],
-        ["Zero-Day (Diffuse)", "scenes/zero-day-(etd)/"],
-        ["Bistro Exterior (Diffuse)", "scenes/bistro-exterior-(etd)/"],
-        ["Bistro Interior (Diffuse)", "scenes/bistro-interior-(etd)/"],
+        ["Cutlery", "scenes/cutlery/"],
+        ["Car", "scenes/car/"],
+        ["Snail", "scenes/snail/"],
+        ["Teaser", "scenes/teaser/"],
     ];
-    new ImageBox(content, data['imageBoxes'], scenes, "time")
-
-    content = document.getElementById("content-sample");
-    scenes = [
-        ["Zero-Day (Glossy)", "scenes/zero-day/"],
-        ["Bistro Exterior (Glossy)", "scenes/bistro-exterior/"],
-        ["Bistro Interior (Glossy)", "scenes/bistro-interior/"],
-        ["Zero-Day (Diffuse)", "scenes/zero-day-(diffuse)/"],
-        ["Bistro Exterior (Diffuse)", "scenes/bistro-exterior-(diffuse)/"],
-        ["Bistro Interior (Diffuse)", "scenes/bistro-interior-(diffuse)/"],
-    ];
-    new ImageBox(content, data['imageBoxes'], scenes, "sample")
+    new ImageBox(content, data['imageBoxes'], scenes, "ggn18")
 }
